423b0d4748ba75d8190f55d6d85b0884ec9d4ad6
cybinon <nikorunikk@gmail.com>
2023-07-06 13:59:46 +0800

    feat: new products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -13,11 +13,11 @@ const HomePage = async () => {
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
+        <Billboard
           data={billboard}
         />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Featured Products" items={products} />
+          <ProductList title="Шинэхэн 🥰" items={products} />
         </div>
       </div>
     </Container>
